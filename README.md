ad8b91bebbc04e12416fd1286511418c2413535e
Prem <prem@wiseai.tech>
2021-09-03 22:43:30 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,78 @@
+<h1 align="center">Realtime Face Anti-Spoofing Detection</h1>
+
+<div align= "center"><img src="https://github.com/Prem95/face-liveness-detector/misc/face.jpg" width="200" height="150"/>
+  <h4>Realtime Face Anti Spoofing Detection with Face Detector to detect REAL and FAKE faces. Built using Tensorflow, Keras and OpenCV</h4>
+</div>
+
+![Python](https://img.shields.io/badge/Python-v3.8+-blue.svg)
+[![contributions welcome](https://img.shields.io/badge/Contributions-Welcome-green.svg?style=flat)](https://github.com/Prem95/face-liveness-detector/issues)
+[![Forks](https://img.shields.io/github/forks/Prem95/face-liveness-detector.svg?logo=github)](https://github.com/Prem95/face-liveness-detector/network/members)
+[![Stargazers](https://img.shields.io/github/stars/Prem95/face-liveness-detector.svg?logo=github)](https://github.com/Prem95/face-liveness-detector/stargazers)
+
+
+## Why Build This?
+Face anti-spoofing systems has lately attracted increasing attention due to its important role in securing face recognition systems from fraudulent attacks. This project aims to provide a starting point in recognising real and fake faces based on a model that is trained with publicly available dataset
+
+
+## Where to use?
+This Face Anti Spoofing detector can possible be used in many different systems that needs realtime facial recognition with facial landmarks. Potentially could be used in security systems, biometrics, attendence systems and etc.
+
+This integrated with hardware systems for application in offices, schools, and public places for various use cases.
+
+## Datasets
+
+The model is trained using Tensorflow from publicly available datasets. Below listed are the data sources that the model is trained on:
+
+CASIA: https://github.com/namtpham/casia2groundtruth
+
+OULU: https://sites.google.com/site/oulunpudatabase/
+
+NUAA: http://parnec.nuaa.edu.cn/_upload/tpl/02/db/731/template731/pages/xtan/NUAAImposterDB_download.html
+
+Please obtain the necessary permissions before using the datasets as above.
+
+## Prerequisites
+
+All the required libraries are included in the file ```requirements.txt```
+
+
+## Installation
+1. Clone the repo
+```
+$ git clone https://github.com/Prem95/face-liveness-detector.git
+```
+
+2. Change your directory to the cloned repo
+```
+$ cd face-liveness-detector
+```
+
+3. Run the following command in your terminal
+```
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+Run the following command in your terminal
+
+```
+$ python main.py
+```
+
+Current Face Anti Spoofing threshold is set at a value of **0.70**. This can be finetuned based on different situations as needed.
+
+## Contribution
+
+Feel free to **file a new issue** with a respective title and description on the the [face-liveness-detector](https://github.com/Prem95/face-liveness-detector/issues) repository.
+
+## Feature Request
+
+Please also submit a pull request for any issues that might appear or any enhancements/features that could make this project perform better. **I would love to review your pull request**!
+
+## Code of Conduct
+
+You can find our Code of Conduct [here](/CODE_OF_CONDUCT.md).
+
+## License
+All rights reserved according to MIT © [Prem Kumar](https://github.com/Prem95/face-liveness-detector/blob/master/LICENSE)
---
+++ utility/video_utils.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+import os
+import sys
+sys.path.insert(1, os.getcwd())
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import cv2
+import tensorflow as tf
+from face_detector import FaceDetector
+from face_det.FaceBoxes import FaceBoxes
+from face_det.TDDFA import TDDFA
+import yaml
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
+class VideoUtils(object):
+
+    def load_facedetector(root_path):
+
+        cfg = yaml.load(open('%s/configs/mb1_120x120.yml' % root_path), Loader=yaml.SafeLoader)
+        cfg['bfm_fp'] = os.path.join(root_path, cfg['bfm_fp'])
+        cfg['checkpoint_fp'] = os.path.join(root_path, cfg['checkpoint_fp'])
+        face_boxes = FaceBoxes(gpu_mode=True)
+        tddfa = TDDFA(gpu_mode=True, **cfg)
+        return face_boxes, tddfa
+
+    def detect_faces(img):
+
+        root_path = "face_det"
+        cfg = yaml.load(open('%s/configs/mb1_120x120.yml' % root_path), Loader=yaml.SafeLoader)
+        cfg['bfm_fp'] = os.path.join(root_path, cfg['bfm_fp'])
+        cfg['checkpoint_fp'] = os.path.join(root_path, cfg['checkpoint_fp'])
+
+        face_boxes = FaceBoxes(gpu_mode=True)
+        faces = face_boxes(img)
+        faces = np.array([[top, right, bottom, left] for left, top, right, bottom, _ in faces]).astype(int).tolist()
+        return faces
+
+    def get_liveness_score(frame, boxes):
+
+        face_location = [[left, top, right, bottom] for top, right, bottom, left in boxes]
+        try:
+            xmin, ymin, xmax, ymax = max(face_location, key=lambda x: (x[2]-x[0]) * (x[3]-x[1]))
+        except ValueError as e:
+            return None
+
+        def get_normal_face(img):
+            return img/255.
+
+        face_img = frame[max(ymin, 0):ymax, max(xmin, 0):xmax].copy()
+        standard_face = np.array(get_normal_face(cv2.resize(face_img, (112, 112))))
+        img_array_expanded = np.expand_dims(standard_face, axis=0)
+        return img_array_expanded
+
+
+    def load_keras_model(model_path):
+        try:
+            model = tf.keras.models.load_model(model_path, custom_objects={"lr": lambda x: x, "SparseCategoricalFocalLoss": lambda x: x})
+            model.status = 0
+        except Exception as e:
+            class empty_model(object):
+                def __init__(self):
+                    self.status = 600
+            model = empty_model()
+            print('[ERROR] Model failed to load. Check file path.')
+            sys.exit()
+        return model
+
+    def get_normal_face(img):
+        return img/255.
+
+    def get_max_face(face_locations):
+        return max(face_locations, key=lambda x: (x[2]-x[0]) * (x[3]-x[1]))
+
+    def cv_draw_landmark(img_ori, pts, box=None, size=1):
+        GREEN = (0, 255, 0)
+        img = img_ori.copy()
+        n = pts.shape[1]
+        if n <= 106:
+            for i in range(n):
+                cv2.circle(img, (int(round(pts[0, i])), int(round(pts[1, i]))), size, GREEN, -1)
+        else:
+            sep = 1
+            for i in range(0, n, sep):
+                cv2.circle(img, (int(round(pts[0, i])), int(round(pts[1, i]))), size, GREEN, 1)
+        if box is not None:
+            left, top, right, bottom = np.round(box).astype(np.int32)
+            left_top = (left, top)
+            right_top = (right, top)
+            right_bottom = (right, bottom)
+            left_bottom = (left, bottom)
+            cv2.line(img, left_top, right_top, GREEN, 1, cv2.LINE_AA)
+            cv2.line(img, right_top, right_bottom, GREEN, 1, cv2.LINE_AA)
+            cv2.line(img, right_bottom, left_bottom, GREEN, 1, cv2.LINE_AA)
+            cv2.line(img, left_bottom, left_top, GREEN, 1, cv2.LINE_AA)
+
+        return img
